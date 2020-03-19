a2772ee0bf8c79b5a4e28af20e6205e10b2a36d0
Ljubomir Kraljević <ljubomir.kraljevic@triple-innovations.com>
2020-03-19 17:29:22 +0100

    WebApp andsolution setup

--- WebApp/Startup.cs
+++ WebApp/Startup.cs
@@ -7,8 +7,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using WebApp.Data;
 
 namespace WebApp
 {
@@ -24,6 +26,7 @@ namespace WebApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+                    
             services.Configure<CookiePolicyOptions>(options =>
             {
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
@@ -31,7 +34,26 @@ namespace WebApp
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-            services.AddAuthorization();
+            //TODO: services.AddAuthentication();
+            //TODO: services.AddAuthorization();
+            bool dataLoggingOption = Configuration.GetValue<bool>("DbProvider:Logging");
+            string DbProviderName = Configuration.GetValue<string>("DbProvider:Name");
+            if (DbProviderName == "Oracle")
+            {
+                services.AddDbContext<ApplicationDbContext>(options =>
+                    options
+                    .UseOracle(Configuration.GetConnectionString("DefaultConnection"))
+                    .EnableSensitiveDataLogging(dataLoggingOption)
+                );
+            }
+            if (DbProviderName == "SQLServer")
+            {
+                services.AddDbContext<ApplicationDbContext>(options =>
+                    options
+                    .UseSqlServer(Configuration.GetConnectionString("DefaultConnection"))
+                    .EnableSensitiveDataLogging(dataLoggingOption)
+                );
+            }
 
             services.AddMvc().AddMvcOptions(o=> o.EnableEndpointRouting=false);
         }
@@ -49,7 +71,6 @@ namespace WebApp
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
