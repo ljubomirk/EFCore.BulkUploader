afba9bbba6896b0a65a74f67eeca170be6781f05
Ljubomir Kraljević <ljubomir.kraljevic@triple-innovations.com>
2020-03-19 17:28:00 +0100

    controllers and views setup

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -5,15 +5,29 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using WebApp.Data;
 using WebApp.Models;
+using WebApp.Services;
 
 namespace WebApp.Controllers
 {
-    [Authorize]
+    //[Authorize]
     public class HomeController : Controller
     {
+        private readonly RepositoryServices _repo;
+
+        public HomeController(ApplicationDbContext context)
+        {
+            _repo = new RepositoryServices(context);
+        }
+
         public IActionResult Index()
         {
+            return View();
+        }
+        public IActionResult Add(string code)
+        {
+            _repo.Add(code);
             return View();
         }
 
