dae99d8c49a9e0db40af9f9c132c39f6fc11847a
Ljubomir Kraljević <ljubomir.kraljevic@triple-innovations.com>
2020-03-24 21:31:26 +0100

    added search function #499

--- AD/Program.cs
+++ AD/Program.cs
@@ -2,13 +2,18 @@
 using System;
 using System.DirectoryServices.Protocols;
 using System.Net;
+using System.Text.Json;
 
 namespace AD
 {
     class Program
     {
-        public static bool ValidateUserByBind(string username, string password, string ldap)
+        static string username;
+        static string password;
+        static string ldap;
+        public static bool ValidateUserByBind(string filter)
         {
+            Console.Out.WriteLine("ValidateUserByBind");
             bool result = true;
             var credentials = new NetworkCredential(username, password);
             var serverId = new LdapDirectoryIdentifier(ldap);
@@ -25,9 +30,39 @@ namespace AD
                 result = false;
             }
 
+            ListUserGroups(conn, filter);
             conn.Dispose();
 
             return result;
+        }
+
+        public static void ListUserGroups(LdapConnection conn, string filter)
+        {
+            Console.Out.WriteLine("ListUserGroups");
+            try
+            {
+                PageResultRequestControl pageRequestControl = new PageResultRequestControl(100);
+
+                // used to retrieve the cookie to send for the subsequent request
+                PageResultResponseControl pageResponseControl;
+                SearchRequest searchRequest = new SearchRequest(username, filter, new SearchScope(), null);
+                searchRequest.Controls.Add(pageRequestControl);
+
+                while (true)
+                {
+                    var response = (SearchResponse)conn.SendRequest(searchRequest);
+                    Console.Out.WriteLine(JsonSerializer.Serialize(response.ResultCode.ToString()));
+                    pageResponseControl = (PageResultResponseControl)response.Controls[0];
+                    if (pageResponseControl.Cookie.Length == 0)
+                        break;
+                    pageRequestControl.Cookie = pageResponseControl.Cookie;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine("Exception message: {0}", ex.Message);
+
+            }
         }
 
         static int Main(string[] args)
@@ -42,18 +77,22 @@ namespace AD
                 ret = 2;
             }
 
-            var usr = args.GetValue(0).ToString();
-            var pwd = args.GetValue(1).ToString();
-            var ldap = args.Length>2 ? args.GetValue(2).ToString() : "wdc2t.simobil.tst";
-            Console.Out.WriteLine("Testing for {0}.", usr);
+            username = args.GetValue(0).ToString();
+            password = args.GetValue(1).ToString();
+            var searchFilter = args.Length > 2 ? args.GetValue(2).ToString() : "(objectCategory=person)(objectClass=user)";
+            ldap = args.Length > 3 ? args.GetValue(3).ToString() : "wdc2t.simobil.tst";
+            Console.Out.WriteLine("Testing for {0}.", username);
             /* Validate credentials */
-            if (!ValidateUserByBind(usr, pwd, ldap))
+            if (!ValidateUserByBind(searchFilter))
             {
-                Console.Error.WriteLine("Can not connect for {0}.", usr);
+                Console.Error.WriteLine("Can not connect for {0}.", username);
                 ret = 1;
             }
+            else
+            {
+                Console.Out.WriteLine("User {0} ready.", username);
+            }
             /* return status */
-            Console.Out.WriteLine("User {0} ready.", usr);
             return ret;
         }
 
