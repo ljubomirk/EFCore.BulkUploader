5bb30654f36fa6be5944e71fda1bea4759a0169e
Ljubomir Kraljević <ljubomir.kraljevic@triple-innovations.com>
2020-03-31 15:41:01 +0200

    added Enabled and Active properties  on Promotion
    #527 Close

--- CouponDatabase/Models/Promotion.cs
+++ CouponDatabase/Models/Promotion.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CouponDatabase.Lifecycle;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -7,8 +8,12 @@ using CouponDatabase.Properties;
 
 namespace CouponDatabase.Models
 {
+    /// <summary>
+    /// Promotion main class
+    /// </summary>
     public class Promotion
     {
+        #region Properties 
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public long Id { get; set; }
         //[System.ComponentModel.DataAnnotations.MaxLength(20,ErrorMessageResourceName = , ErrorMessageResourceType = 1]
@@ -17,9 +22,34 @@ namespace CouponDatabase.Models
         [Display(Name = "Promotion.Code", ResourceType = typeof(Resources))]
         [DataType(DataType.Text)]
         public string Code { get; set; }
-
+        public Boolean Enabled { get; set; }
         public DateTime ValidFrom { get; set; }
         public DateTime ValidTo { get; set; }
 
+        #endregion
+        #region construction
+        public Promotion()
+        {
+
+        }
+
+        /* Active is virtual value, read from other values */
+        public Boolean Active
+        {
+            get => this.GetActive();
+        }
+        /// <summary>
+        /// Returns Active property for Promotion
+        /// </summary>
+        /// <returns>Boolean value</returns>
+        private Boolean GetActive()
+        {
+            var pr1 = (DateTime.Now.CompareTo(ValidFrom) >= 0) ? true : false;
+            var pr2 = (DateTime.Now.CompareTo(ValidTo) < 0) ? true : false;
+
+            return Enabled && pr1 && pr2;
+        }
+
+        #endregion
     }
 }
