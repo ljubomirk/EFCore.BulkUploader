c94426591fca766b36ef4b9fbb3a96e155e5744d
Luka Vuković <luka.vukovic@triple-innovations.com>
2020-04-03 13:50:01 +0200

    Added [GET] funtions to repo
    #547

--- WebApp/Services/RepositoryService.cs
+++ WebApp/Services/RepositoryService.cs
@@ -2,6 +2,8 @@
 using Microsoft.Extensions.Options;
 using WebApp.Data;
 using CouponDatabase.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace WebApp.Services
 {
@@ -18,6 +20,33 @@ namespace WebApp.Services
             Coupon coupon = new Coupon(code, null, null, CouponDatabase.Lifecycle.CouponStatus.Created, null);
             Context.Coupon.Add(coupon);
             Context.SaveChanges();
+        }
+
+        public List<Promotion> GetAllPromotions()
+        {
+            return Context.Promotion.ToList<Promotion>();
+        }
+        public List<Property> GetAllProperties()
+        {
+            return Context.Properties.ToList<Property>();
+        }
+        public List<Property> GetAllPromotionProperties(long idPromotion)
+        {
+            return Context.PromotionProperties.Where(x=>x.PromotionId==idPromotion).Select(c => c.Properties).Distinct().ToList<Property>();
+        }
+        public List<AwardChannel> GetAllPromotionAwardChannels(long idPromotion)
+        {
+            return Context.PromotionAwardChannels.Where(x => x.PromotionId == idPromotion).Select(c => c.AwardChannel).Distinct().ToList<AwardChannel>();
+        }
+        public List<IssuerChannel> GetAllPromotionIssuerChannels(long idPromotion)
+        {
+            return Context.PromotionIssuerChannels.Where(x => x.PromotionId == idPromotion).Select(c => c.IssuerChannel).Distinct().ToList<IssuerChannel>();
+        }
+        public bool CreatePromotion(Promotion promotion)
+        {
+            Context.Promotion.Add(promotion);
+            Context.SaveChanges();
+            return true;
         }
     }
 }
