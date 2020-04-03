4ee6081610a8c9809a94d23daf8c2d7ed8242bec
Ljubomir Kraljević <ljubomir.kraljevic@triple-innovations.com>
2020-04-03 18:08:35 +0200

    Added CheckedItem to support view model
    #556 Prepared

---
+++ WebApp/ViewModels/CheckedItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp.ViewModels
+{
+    /// <summary>
+    /// Used for ViewModel to support checkbox fields
+    /// </summary>
+    public class CheckedItem
+    {
+        public long Id { get; set; }
+        public string Label { get; set; }
+        public bool Checked { get; set; }
+    }
+}
