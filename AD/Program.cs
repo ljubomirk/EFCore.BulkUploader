3c5824ec63685365e6ad44620c9c36d60015b523
Ljubomir Kraljević <ljubomir.kraljevic@triple-innovations.com>
2020-04-07 11:37:03 +0200

    prepared checking of user groups on LDAP #538 Done

--- AD/Program.cs
+++ AD/Program.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.IdentityModel.Clients.ActiveDirectory;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
 using System;
+using System.DirectoryServices.AccountManagement;
 using System.DirectoryServices.Protocols;
 using System.Net;
 using System.Text.Json;
@@ -11,6 +13,9 @@ namespace AD
         static string username;
         static string password;
         static string ldap;
+        static string dname;
+        static string domain;
+
         public static bool ValidateUserByBind(string filter)
         {
             Console.Out.WriteLine("ValidateUserByBind");
@@ -22,6 +27,7 @@ namespace AD
             try
             {
                 conn.Bind();
+                conn.Timeout = TimeSpan.FromSeconds(8);
             }
             catch (Exception ex)
             {
@@ -30,38 +36,67 @@ namespace AD
                 result = false;
             }
 
-            ListUserGroups(conn, filter);
+            SearchGroups(conn, filter);
             conn.Dispose();
 
             return result;
         }
 
-        public static void ListUserGroups(LdapConnection conn, string filter)
+        public static void ListUserGroups()
         {
             Console.Out.WriteLine("ListUserGroups");
             try
             {
-                PageResultRequestControl pageRequestControl = new PageResultRequestControl(100);
-
-                // used to retrieve the cookie to send for the subsequent request
-                PageResultResponseControl pageResponseControl;
-                SearchRequest searchRequest = new SearchRequest(username, filter, new SearchScope(), null);
-                searchRequest.Controls.Add(pageRequestControl);
-
-                while (true)
+                Console.Out.WriteLine("UserPrincipal");
+                UserPrincipal user = UserPrincipal.FindByIdentity(new PrincipalContext(ContextType.Domain, domain), username);
+                Console.Out.WriteLine("UserPrincipal called");
+                foreach (GroupPrincipal group in user.GetGroups())
                 {
-                    var response = (SearchResponse)conn.SendRequest(searchRequest);
-                    Console.Out.WriteLine(JsonSerializer.Serialize(response.ResultCode.ToString()));
-                    pageResponseControl = (PageResultResponseControl)response.Controls[0];
-                    if (pageResponseControl.Cookie.Length == 0)
-                        break;
-                    pageRequestControl.Cookie = pageResponseControl.Cookie;
+                    Console.Out.WriteLine("Group: " + group);
                 }
+
             }
             catch (Exception ex)
             {
                 Console.Error.WriteLine("Exception message: {0}", ex.Message);
 
+            }
+        }
+        public static void SearchGroups(LdapConnection conn, string filter)
+        {
+            Console.Out.WriteLine("SearchGroups");
+            try
+            {
+                Console.Out.WriteLine("PageRequest");
+                PageResultRequestControl pageRequestControl = new PageResultRequestControl(100);
+                Console.Out.WriteLine("PageRequest called");
+
+                // used to retrieve the cookie to send for the subsequent request
+                Console.Out.WriteLine("SearchRequest");
+                SearchRequest searchRequest = new SearchRequest();
+                searchRequest.DistinguishedName = dname;
+                searchRequest.Filter = filter;
+                searchRequest.Controls.Add(pageRequestControl);
+
+                Console.Out.WriteLine("SearchRequest call");
+                var response = (SearchResponse)conn.SendRequest(searchRequest);
+                Console.Out.WriteLine("SearchRequest called");
+                Console.Out.WriteLine(JsonSerializer.Serialize(response.ResultCode.ToString()));
+                foreach(SearchResultEntry entries in response.Entries)
+                {
+                    System.Console.WriteLine("Dname: {0}", entries.DistinguishedName);
+                    foreach (string name in entries.Attributes.AttributeNames) {
+                        DirectoryAttribute attribute = entries.Attributes[name];
+                        foreach(string value in attribute.GetValues(name.GetType()))
+                            System.Console.WriteLine("{0}={1}", name, value);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine("Exception message: {0} {1}", ex.Message, ex.StackTrace);
+                if(ex.InnerException!=null)
+                    Console.Error.WriteLine("Exception message: {0}", ex.InnerException.Message);
             }
         }
 
@@ -71,6 +106,18 @@ namespace AD
             var authContext = new AuthenticationContext("https://login.microsoftonline.com/common");
             var result = authContext.AcquireTokenAsync("https://graph.microsoft.com",
                  cred );*/
+
+            /* 
+             * Example usages:
+             *  - Get group Coupon Managers
+             *      AD.exe sizdit Ljubo.On20 "(&(objectClass=group)(sAMAccountName=Camp Managers))" "DC=simobil,DC=tst"
+             *  - Get users by group
+             *      AD.exe sizdit Ljubo.On20 "(&(objectClass=user)(memberOf=CN=Camp Designers,OU=Groups,DC=simobil,DC=tst))" "DC=simobil,DC=tst"
+             *  - Get user data
+             *      AD.exe sizdit Ljubo.On20 "(&(objectClass=user)(sAMAccountName=sizdit))" "DC=simobil,DC=tst"
+             *  - Is user member of group
+             *      AD.exe sizdit Ljubo.On20 "(&(objectClass=user)(sAMAccountName=lea)(memberOf=CN=Camp Designers,OU=Groups,DC=simobil,DC=tst))" "DC=simobil,DC=tst"
+             */        
             int ret=0;
             if (args.Length < 2) {
                 Console.Error.WriteLine("Invalid parameters: AD username password");
@@ -79,9 +126,13 @@ namespace AD
 
             username = args.GetValue(0).ToString();
             password = args.GetValue(1).ToString();
-            var searchFilter = args.Length > 2 ? args.GetValue(2).ToString() : "(objectCategory=person)(objectClass=user)";
-            ldap = args.Length > 3 ? args.GetValue(3).ToString() : "wdc2t.simobil.tst";
+            var searchFilter = args.Length > 2 ? args.GetValue(2).ToString() : "(objectClass=*)";
+            dname = args.Length > 3 ? args.GetValue(3).ToString() : "CN=Users,DN=simobil,DN=test";
+            domain = args.Length > 5 ? args.GetValue(4).ToString() : "simobil.tst";
+            ldap = args.Length > 4 ? args.GetValue(5).ToString() : "wdc2t.simobil.tst:389";
             Console.Out.WriteLine("Testing for {0}.", username);
+            /* List groups */
+            ListUserGroups();
             /* Validate credentials */
             if (!ValidateUserByBind(searchFilter))
             {
