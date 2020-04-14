80304d42f6a75d644dee9d875608b038717f5184
Siniša <skrunic@users.noreply.github.com>
2020-04-14 17:50:00 +0200

    Add CouponFilter class.

---
+++ WebApp/ViewModels/CouponFilters.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using CouponDatabase.Models;
+using System.Collections.Generic;
+using WebApp.Services;
+
+namespace WebApp.ViewModels
+{
+    public class CouponFilters : Filters
+    {
+        public List<CheckedItem> IssuerChannels { get; set; }
+        public List<CheckedItem> AwardChannels { get; set; }
+        public List<CheckedItem> CurrentCouponStatus { get; set; }
+
+        public CouponFilters()
+        {
+            ShowActive = true;
+            ShowInactive = false;
+            IssuerChannels = new List<CheckedItem>();
+            AwardChannels = new List<CheckedItem>();
+            CurrentCouponStatus = new List<CheckedItem>();
+            Properties = new List<CheckedItem>();
+        }
+    }
+}
