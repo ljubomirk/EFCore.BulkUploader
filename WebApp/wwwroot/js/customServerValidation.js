4706d589c642011068631f43dfcfa6ea1a83cc32
Dino Kraljeta <pockshocks@gmail.com>
2020-04-16 13:11:43 +0200

    design for lifecycle

---
+++ WebApp/wwwroot/js/customServerValidation.js
@@ -0,0 +1,26 @@
+﻿$(document).ready(function () {
+    // server-side validation
+    var errHolder = $('.validation-error')
+    if (errHolder) {
+        errHolder.tooltip();
+        errHolder.siblings("input").addClass("invalid");
+        errHolder.siblings("input")
+            .mouseenter(function () {
+                $(this).siblings(".validation-error").tooltip('open');
+
+            }).mouseleave(function () {
+                $(this).siblings(".validation-error").tooltip('close');
+
+            }).change(function () {
+                $(this).removeClass("invalid");
+                var icon = $(this).siblings(".validation-error");
+
+                if ($(this).hasClass("calendar")) {
+                    console.log(icon);
+                    icon.removeClass("validation-error").html("event");
+                } else {
+                    icon.remove();
+                }
+            })
+    }
+})
