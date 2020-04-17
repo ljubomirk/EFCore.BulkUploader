9fc77154285e03eac56992b2429e2b8781169afe
Siniša <skrunic@users.noreply.github.com>
2020-04-18 00:38:53 +0200

    Filter date format update.

--- WebApp/ViewModels/Filters.cs
+++ WebApp/ViewModels/Filters.cs
@@ -2,6 +2,7 @@
 using CouponDatabase.Models;
 using System.Collections.Generic;
 using WebApp.Services;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApp.ViewModels
 {
@@ -10,7 +11,11 @@ namespace WebApp.ViewModels
         public string Code { get; set; }
         public bool ShowActive { get; set; }
         public bool ShowInactive { get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
         public DateTime ValidFrom { get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}", ApplyFormatInEditMode = true)]
+        [DataType(DataType.Date)]
         public DateTime ValidTo { get; set; }
         public List<CheckedItem> Properties { get; set; }
 
