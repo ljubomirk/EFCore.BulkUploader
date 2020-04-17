6facb3272ccfb72833ce79a3edefb3a791ba3f61
Dino Kraljeta <pockshocks@gmail.com>
2020-04-17 06:16:53 +0200

    test controller for administration

---
+++ WebApp/Controllers/AdministrationController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApp.ViewModels;
+
+namespace WebApp.Controllers
+{
+    public class AdministrationController : Controller
+    {
+        public IActionResult Users()
+        {
+            ContextData user = new ContextData();
+            return View("AdministrationUsers", user);
+        }
+        public IActionResult Channels()
+        {
+            ContextData user = new ContextData();
+            return View("AdministrationChannels", user);
+        }
+        public IActionResult AccessHistory()
+        {
+            ContextData user = new ContextData();
+            return View("AdministrationAccessHistory", user);
+        }
+    }
+}
