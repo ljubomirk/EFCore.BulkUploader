cfaf2143fb70145e6d444b37e5031356a707dff1
Siniša <skrunic@users.noreply.github.com>
2020-04-27 21:41:47 +0200

    Add LifecycleManagementModel for session storage. Add SessionExtensions to allow object storage into state.

---
+++ WebApp/Services/LifecycleManagementModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApp.ViewModels;
+
+namespace WebApp.Services
+{
+    public class LifecycleManagementModel
+    {
+        public PromotionFilter PromotionFilter { get; set; }
+        public CouponFilters CouponFilter { get; set; }
+        public string SelectedPromoCode { get; set; }
+        public string SelectedCouponSeries { get; set; }
+        public List<CheckedCouponItem> CouponItems { get; set; }
+    }
+}
