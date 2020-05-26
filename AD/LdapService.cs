2dbed26c2e6d910f72c777b9635bf9760652f033
Ljubomir Kraljević <ljubomir.kraljevic@triple-innovations.com>
2020-05-26 10:29:54 +0200

    fix  authorization on LDAP

--- AD/LdapService.cs
+++ AD/LdapService.cs
@@ -21,24 +21,41 @@ namespace TripleI.ActiveDirectory
 
         public async Task Invoke(HttpContext context)
         {
-            string username = Environment.GetEnvironmentVariable("USERNAME") ?? Environment.GetEnvironmentVariable("USER");
+            string fullUsername = context.User.Identity.Name ?? Environment.GetEnvironmentVariable("USER");
+            string username = fullUsername;
+            if (fullUsername.Split('\\').Length>1)
+                username = fullUsername.Split('\\')[1];
             _logger.LogInformation("LdapAuthorization for {0}.", username);
             context.Request.Headers.TryGetValue("Authorization", out var authorization);
             _logger.LogInformation("LdapService for authorization:{0}!", string.Join("",authorization.ToArray()));
             _logger.LogInformation("LdapService for username:{0}!", username);
-            LdapAuthorization ad = new LdapAuthorization("coupont", "Rok Seliskar 20", "wdc2t.simobil.tst:389", "DC=simobil,DC=test", _logger);
-            ad.Connect();
-            if (ad.SearchUsers(username).Count == 0) { 
-                context.Request.Headers.Add("ApplicationGroup", new Microsoft.Extensions.Primitives.StringValues("None"));
-                await _next.Invoke(context);
+            if (context.Request.Headers.ContainsKey("ApplicationUser")) {
+                context.Request.Headers.Remove("ApplicationUser");
+                context.Request.Headers.Add("ApplicationUser", new Microsoft.Extensions.Primitives.StringValues(username));
             }
             else
-            {
-                if (ad.SearchUsersInGroup("Coupon Manager").Count == 1)
-                    context.Request.Headers.Add("ApplicationGroup", new Microsoft.Extensions.Primitives.StringValues("Manager"));
-                if (ad.SearchUsersInGroup("Coupon Administrator").Count == 1)
-                    context.Request.Headers.Add("ApplicationGroup", new Microsoft.Extensions.Primitives.StringValues("Administrator"));
-                await _next.Invoke(context);
+            { 
+                if (context.Request.Headers.ContainsKey("ApplicationGroup"))
+                    context.Request.Headers.Remove("ApplicationGroup");
+                LdapAuthorization ad = new LdapAuthorization("coupont", "Rok Seliskar 20", "wdc2t.simobil.tst:389", "DC=simobil,DC=tst", _logger);
+                ad.Connect();
+                if (ad.SearchUsers(username).Count == 0) {
+                    _logger.LogInformation("LdapService None:{0}!", username);
+                    context.Request.Headers.Add("ApplicationGroup", new Microsoft.Extensions.Primitives.StringValues("None"));
+                    await _next.Invoke(context);
+                }
+                else
+                {
+                    if (ad.IsUserInGroup(username, "Coupon Users").Count == 1) {
+                        _logger.LogInformation("LdapService Manager:{0}!", username);
+                        context.Request.Headers.Add("ApplicationGroup", new Microsoft.Extensions.Primitives.StringValues("Manager"));
+                    }
+                    if (ad.IsUserInGroup(username, "Coupon Admins").Count == 1) {
+                        _logger.LogInformation("LdapService Administrator:{0}!", username);
+                        context.Request.Headers.Add("ApplicationGroup", new Microsoft.Extensions.Primitives.StringValues("Administrator"));
+                    }
+                    await _next.Invoke(context);
+                }
             }
             _logger.LogInformation(message: "LdapAuth done {DateTime}. ", DateTime.Now.ToLocalTime().ToString());
         }
