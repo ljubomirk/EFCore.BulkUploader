85e4a04de216bef5acea5b4ed1bd40c1585610d3
Ljubomir Kraljević <ljubomir.kraljevic@triple-innovations.com>
2020-06-14 13:37:38 +0200

    setup and test data

--- DB/OracleSetupDef.sql
+++ DB/OracleSetupDef.sql
@@ -1,38 +1,41 @@
 BEGIN
 INSERT INTO "AWARD_CHANNEL" ("ID", "NAME")
-VALUES (4, N'Telesales');
+VALUES (4, 'Telesales');
 INSERT INTO "AWARD_CHANNEL" ("ID", "NAME")
-VALUES (3, N'SelfCare');
+VALUES (3, 'SelfCare');
 INSERT INTO "AWARD_CHANNEL" ("ID", "NAME")
-VALUES (1, N'POS');
+VALUES (1, 'POS');
 INSERT INTO "AWARD_CHANNEL" ("ID", "NAME")
-VALUES (5, N'Webshop');
+VALUES (5, 'Webshop');
+COMMIT;
 END;
 /
 
 BEGIN
 INSERT INTO "ISSUER_CHANNEL" ("ID", "NAME")
-VALUES (2, N'Salesforce');
+VALUES (2, 'Salesforce');
 INSERT INTO "ISSUER_CHANNEL" ("ID", "NAME")
-VALUES (3, N'SelfCare');
+VALUES (3, 'SelfCare');
 INSERT INTO "ISSUER_CHANNEL" ("ID", "NAME")
-VALUES (4, N'Telesales');
+VALUES (4, 'Telesales');
 INSERT INTO "ISSUER_CHANNEL" ("ID", "NAME")
-VALUES (5, N'Webshop');
+VALUES (5, 'Webshop');
 INSERT INTO "ISSUER_CHANNEL" ("ID", "NAME")
-VALUES (1, N'POS');
+VALUES (1, 'POS');
+COMMIT;
 END;
 /
 
 
 BEGIN
 INSERT INTO "PROPERTY" ("ID", "NAME")
-VALUES (2, N'NamedHolders');
+VALUES (2, 'NamedHolders');
 INSERT INTO "PROPERTY" ("ID", "NAME")
-VALUES (4, N'HolderIsOnlyConsumer');
+VALUES (4, 'HolderIsOnlyConsumer');
 INSERT INTO "PROPERTY" ("ID", "NAME")
-VALUES (5, N'AllowMultipleRedeems');
+VALUES (5, 'AllowMultipleRedeems');
 INSERT INTO "PROPERTY" ("ID", "NAME")
-VALUES (6, N'AllowCouponSeries');
+VALUES (6, 'AllowCouponSeries');
+COMMIT;
 END;
 /
