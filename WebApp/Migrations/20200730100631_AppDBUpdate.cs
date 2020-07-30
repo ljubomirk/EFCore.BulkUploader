2461323fd759032cae8f4beb1bb2cc181e45c4fd
Tomislav Krmek <tomislav.krmek@triple-innovations.com>
2020-07-30 12:08:23 +0200

    Add new migration including Promotion "NAME" property #888

---
+++ WebApp/Migrations/20200730100631_AppDBUpdate.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebApp.Migrations
+{
+    public partial class AppDBUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "NAME",
+                table: "PROMOTION",
+                maxLength: 40,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NAME",
+                table: "PROMOTION");
+        }
+    }
+}
