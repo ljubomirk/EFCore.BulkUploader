1db14cef5cf6a22939c50ee4f9c24101fa6a1e1f
Iva Bokšić <iva.boksic@triple-innovations.com>
2020-08-07 11:13:54 +0200

    added names in Resources file for all table headers #902

--- CouponDatabase/Models/Coupon.cs
+++ CouponDatabase/Models/Coupon.cs
@@ -1,4 +1,5 @@
 ﻿using CouponDatabase.Lifecycle;
+using CouponDatabase.Properties;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
@@ -35,40 +36,52 @@ namespace CouponDatabase.Models
         }
 
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name = "Coupon_Id", ResourceType = typeof(Resources))]
         public long Id { get; set; }
         //[System.ComponentModel.DataAnnotations.MaxLength(20,ErrorMessageResourceName = , ErrorMessageResourceType = 1]
         [DataType(DataType.Text)]
 
         [MaxLength(40)]
+        [Display(Name = "Coupon_Code", ResourceType = typeof(Resources))]
         public string Code { get; set; }
 
         [MaxLength(20)]
+        [Display(Name = "Coupon_Holder", ResourceType = typeof(Resources))]
         public string Holder { get; set; }
         [MaxLength(20)]
+        [Display(Name = "Coupon_User", ResourceType = typeof(Resources))]
         public string User { get; set; }
-
+        [Display(Name = "Coupon_Status", ResourceType = typeof(Resources))]
         public int Status { get; set; }
         [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}", ApplyFormatInEditMode = true)]
         [DataType(DataType.Date)]
+        [Display(Name = "Date_AquireFrom", ResourceType = typeof(Resources))]
         public Nullable<DateTime> AquireFrom { get; set; }
         [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}", ApplyFormatInEditMode = true)]
         [DataType(DataType.Date)]
+        [Display(Name = "Date_AquireTo", ResourceType = typeof(Resources))]
         public Nullable<DateTime> AquireTo { get; set; }
 
         [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}", ApplyFormatInEditMode = true)]
         [DataType(DataType.Date)]
+        [Display(Name = "Date_AwardFrom", ResourceType = typeof(Resources))]
         public Nullable<DateTime> AwardFrom { get; set; }
         [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}", ApplyFormatInEditMode = true)]
         [DataType(DataType.Date)]
+        [Display(Name = "Date_AwardTo", ResourceType = typeof(Resources))]
         public Nullable<DateTime> AwardTo { get; set; }
-
+        [Display(Name = "Promotion_Id", ResourceType = typeof(Resources))]
         public long PromotionId { get; set; }
         public Promotion Promotion { get; set; }
+        [Display(Name = "Coupon_Series", ResourceType = typeof(Resources))]
         public int CouponSeries { get; set; }
+        [Display(Name = "Coupon_MaximumRedeem", ResourceType = typeof(Resources))]
         public int MaxRedeemNo { get; set; }
+        [Display(Name = "Promotion_StatusEnabled", ResourceType = typeof(Resources))]
         public Boolean Enabled { get; set; }
 
         /* Active is virtual value, read from other values */
+        [Display(Name = "Promotion_Active", ResourceType = typeof(Resources))]
         public Boolean Active
         {
             get => this.GetActive();
