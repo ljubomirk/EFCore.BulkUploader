7aef584b3b8a0b99be6fbd09d52fa140bd753c71
Iva Bokšić <iva.boksic@triple-innovations.com>
2020-08-13 14:15:42 +0200

    implemented unit-tests for PromotionAPI.Get that are checking logic befind Get function in APIServices.cs #915

--- TestAPI/UnitTestModel.cs
+++ TestAPI/UnitTestModel.cs
@@ -29,13 +29,13 @@ namespace TestAPI
         }
 
         [Test]
-        public void TestPromotionFilter()
+        public void TestPromotionFilter_1()
         {
             //setup
             List<Promotion> promoList = new List<Promotion>();
             Promotion promo = new Promotion()
             {
-                Code = "123",
+                Name = "123",
                 ValidFrom = DateTime.Now.AddMonths(-1),
                 ValidTo = DateTime.Now.AddMonths(1)
             };
@@ -47,6 +47,107 @@ namespace TestAPI
             if (ValidFrom.HasValue && ValidTo.HasValue && ValidFrom < ValidTo)
             {
                 Assert.IsTrue(promoList.FindAll(p => (p.ValidTo.HasValue) ? p.ValidTo > ValidFrom : true).Count > 0);
+            }
+        }
+
+        [Test]
+        public void TestPromotionFilter_2()
+        {
+            //setup
+            List<Promotion> promoList = new List<Promotion>();
+            Promotion promo = new Promotion()
+            {
+                Name = "1234",
+                ValidFrom = new DateTime (2020-01-01),
+                ValidTo = null
+            };
+            promoList.Add(promo);
+            DateTime? ValidFrom = DateTime.Now.AddMonths(-2);
+            DateTime? ValidTo = null;
+            //test
+            // if upper limit valid
+            if (ValidFrom.HasValue && ValidTo.HasValue && ValidFrom < ValidTo)
+            {
+                Assert.IsTrue(promoList.FindAll(x => x.ValidFrom <= ValidFrom && (x.ValidTo >= ValidFrom || x.ValidTo == null)
+                                                                    || (x.ValidFrom >= ValidFrom)
+                                                                    || (x.ValidFrom == null && (x.ValidTo == null || x.ValidTo >= ValidFrom))).Count > 0);
+            }
+        }
+
+
+        [Test]
+        public void TestPromotionFilter_3()
+        {
+            //setup
+            List<Promotion> promoList = new List<Promotion>();
+            Promotion promo = new Promotion()
+            {
+                Name = "12345",
+                ValidFrom = null,
+                ValidTo = DateTime.Now.AddMonths(-2)
+            };
+            promoList.Add(promo);
+            DateTime? ValidFrom = null;
+            DateTime? ValidTo =new DateTime(2020 - 01 - 01) ;
+            //test
+            // if upper limit valid
+            if (ValidFrom.HasValue && ValidTo.HasValue && ValidFrom < ValidTo)
+            {
+                Assert.IsTrue(promoList.FindAll(x => (x.ValidFrom <= ValidTo)
+                                                                    || (x.ValidTo >= ValidTo)
+                                                                    || (x.ValidFrom == null)).Count > 0);
+            }
+        }
+
+        [Test]
+        public void TestPromotionFilter_4()
+        {
+            //setup
+            List<Promotion> promoList = new List<Promotion>();
+            Promotion promo = new Promotion()
+            {
+                Name = "123456",
+                ValidFrom = null,
+                ValidTo = DateTime.Now.AddMonths(-2)
+            };
+            promoList.Add(promo);
+            DateTime? ValidFrom = new DateTime(2020 - 01 - 01);
+            DateTime? ValidTo = new DateTime(2020 - 10 - 01);
+            //test
+            // if upper limit valid
+            if (ValidFrom.HasValue && ValidTo.HasValue && ValidFrom < ValidTo)
+            {
+                Assert.IsTrue(promoList.FindAll(x => (x.ValidFrom <= ValidFrom && (x.ValidTo >= ValidTo || x.ValidTo == null))
+                                                            || (x.ValidFrom >= ValidFrom && x.ValidTo <= ValidTo)
+                                                            || (x.ValidFrom >= ValidFrom && (x.ValidTo == null || x.ValidTo >= ValidTo) && x.ValidFrom <= ValidTo)
+                                                            || (x.ValidFrom <= ValidFrom && x.ValidTo <= ValidTo && x.ValidTo >= ValidFrom)
+                                                            || (x.ValidFrom == null && (x.ValidTo == null || x.ValidTo >= ValidFrom))).Count > 0);
+            }
+        }
+
+        [Test]
+        public void TestPromotionFilter_5()
+        {
+            //setup
+            List<Promotion> promoList = new List<Promotion>();
+            Promotion promo = new Promotion()
+            {
+                Name = "123456",
+                ValidFrom = new DateTime(2020 - 10 - 01),
+                ValidTo = new DateTime(2020 - 10 - 01)
+        };
+            promoList.Add(promo);
+            DateTime? ValidFrom = new DateTime(2020 - 01 - 01);
+            DateTime? ValidTo = new DateTime(2020 - 10 - 01);
+            //test
+            // if upper limit valid
+            if (ValidFrom.HasValue && ValidTo.HasValue && ValidFrom < ValidTo)
+            {
+                Assert.IsTrue(promoList.FindAll(x => (x.ValidFrom <= ValidFrom && (x.ValidTo >= ValidTo || x.ValidTo == null))
+                                                            || (x.ValidFrom >= ValidFrom && x.ValidTo <= ValidTo)
+                                                            || (x.ValidFrom >= ValidFrom && (x.ValidTo == null || x.ValidTo >= ValidTo) && x.ValidFrom <= ValidTo)
+                                                            || (x.ValidFrom <= ValidFrom && x.ValidTo <= ValidTo && x.ValidTo >= ValidFrom)
+                                                            || (x.ValidFrom == null && (x.ValidTo == null || x.ValidTo >= ValidFrom))).Count > 0);
             }
         }
 
