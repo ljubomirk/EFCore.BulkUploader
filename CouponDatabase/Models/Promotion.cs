2ef9d932ef9000be3dfb68ea9b4fc42984d53610
Iva Bokšić <iva.boksic@triple-innovations.com>
2020-08-17 12:25:48 +0200

    modified GetActive function in Promotion.cs #917

--- CouponDatabase/Models/Promotion.cs
+++ CouponDatabase/Models/Promotion.cs
@@ -72,8 +72,8 @@ namespace CouponDatabase.Models
         /// <returns>Boolean value</returns>
         private Boolean GetActive()
         {
-            var pr1 =ValidFrom != null? (DateTime.Now.CompareTo(ValidFrom) >= 0) ? true : false : false;
-            var pr2 = ValidTo != null? (DateTime.Now.CompareTo(ValidTo) < 0) ? true : false : true;
+            var pr1 =ValidFrom != null? (DateTime.Now.CompareTo(ValidFrom) >= 0) ? true : false : true;
+            var pr2 = ValidTo != null? (DateTime.Now.CompareTo(ValidTo) <= 0) ? true : false : true;
 
             return Enabled && pr1 && pr2;
         }
